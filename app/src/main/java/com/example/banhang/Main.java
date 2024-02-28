00eaa11674425c12e6943779ecbcba146f9e5450
Nguyễn Tùng <nguyentung@MacBook-Air-cua-Nguyen.local>
2024-02-28 23:48:17 +0700

    commit b2

--- app/src/main/java/com/example/banhang/Main.java
+++ app/src/main/java/com/example/banhang/Main.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ViewFlipper;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends AppCompatActivity {
     Toolbar toolbar;
@@ -18,6 +28,7 @@ public class Main extends AppCompatActivity {
     RecyclerView recyclerView;
     NavigationView navigationView;
     ListView listView;
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -25,6 +36,8 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         anhxa();
+        ActionViewFlipper();
+        ActisonBar();
     }
     private void anhxa(){
         toolbar = findViewById(R.id.toolbarMain);
@@ -33,6 +46,38 @@ public class Main extends AppCompatActivity {
         listView = findViewById(R.id.listviewMain);
         viewFlipper = findViewById(R.id.viewflipperMain);
         recyclerView = findViewById(R.id.recyclerviewMain);
+        drawerLayout = findViewById(R.id.drawerlayoutMain);
+
+    }
+    private void ActisonBar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_launcher_background);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+    private void ActionViewFlipper(){
+        List<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangquangcao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        mangquangcao.add("https://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
+        for (int i = 0 ; i < mangquangcao.size() ; i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+
 
     }
 }
